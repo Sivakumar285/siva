388ed75b2300ad7929a11759fba1140492dbac0c
Sivakumar285 <139198540+Sivakumar285@users.noreply.github.com>
2023-07-18 09:38:34 +0530

    Update BFS  search.c

--- BFS  search.c
+++ BFS  search.c
@@ -1,13 +1,9 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
-
 struct node {
 	int key;
 	struct node *left, *right;
 };
-
 
 struct node* newNode(int item)
 {
@@ -17,39 +13,24 @@ struct node* newNode(int item)
 	temp->left = temp->right = NULL;
 	return temp;
 }
-
 struct node* insert(struct node* node, int key)
 {
-
-	if (node == NULL)
-		return newNode(key);
-
-
+if (node == NULL)
+return newNode(key);
 	if (key < node->key)
 		node->left = insert(node->left, key);
 	else if (key > node->key)
 		node->right = insert(node->right, key);
-
-
 	return node;
 }
-
-
 struct node* search(struct node* root, int key)
 {
-
 	if (root == NULL || root->key == key)
 		return root;
-
-
 	if (root->key < key)
 		return search(root->right, key);
-
-
 	return search(root->left, key);
 }
-
-
 int main()
 {
 	struct node* root = NULL;
@@ -61,18 +42,12 @@ int main()
 	insert(root, 60);
 	insert(root, 80);
 
-
 	int key = 6;
-
-
 	if (search(root, key) == NULL)
 		printf("%d not found\n", key);
 	else
-		printf("%d found\n", key);
-
-	key = 60;
-
-
+		printf("%d found\n", key)
+	key = 60
 	if (search(root, key) == NULL)
 		printf("%d not found\n", key);
 	else
