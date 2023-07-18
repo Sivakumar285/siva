1cb2739bd0e13a9f544c54f6c64e071693d744ac
Sivakumar285 <139198540+Sivakumar285@users.noreply.github.com>
2023-07-18 09:58:30 +0530

    Update BFS  insert.c

--- BFS  insert.c
+++ BFS  insert.c
@@ -5,8 +5,6 @@ struct node {
 	int key;
 	struct node *left, *right;
 };
-
- 
 struct node* newNode(int item)
 {
 	struct node* temp
@@ -14,9 +12,7 @@ struct node* newNode(int item)
 	temp->key = item;
 	temp->left = temp->right = NULL;
 	return temp;
-}
-
- 
+} 
 void inorder(struct node* root)
 {
 	if (root != NULL) {
@@ -25,24 +21,16 @@ void inorder(struct node* root)
 		inorder(root->right);
 	}
 }
-
 struct node* insert(struct node* node, int key)
 {
- 
-	if (node == NULL)
-		return newNode(key);
-
-	 
+ if (node == NULL)
+ return newNode(key);
 	if (key < node->key)
 		node->left = insert(node->left, key);
 	else if (key > node->key)
 		node->right = insert(node->right, key);
-
-	 
 	return node;
 }
-
- 
 int main()
 {
  
